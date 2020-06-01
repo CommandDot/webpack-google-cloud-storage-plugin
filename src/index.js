d5656cc2e95d908a42351855df6dc9bc0f6eec21
Emile Fugulin <code@efugulin.com>
2020-06-01 17:37:55 -0400

    Run prettier

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import Promise from 'bluebird';
-import PropTypes from 'prop-types';
-import merge from 'lodash.merge';
-import gcs from '@google-cloud/storage';
-import path from 'path';
-import { pick } from './utils';
+import Promise from "bluebird";
+import PropTypes from "prop-types";
+import merge from "lodash.merge";
+import gcs from "@google-cloud/storage";
+import path from "path";
+import { pick } from "./utils";
 
-const recursive = Promise.promisify(require('recursive-readdir'));
+const recursive = Promise.promisify(require("recursive-readdir"));
 
-const pluginName = 'WebpackGoogleCloudStoragePlugin';
+const pluginName = "WebpackGoogleCloudStoragePlugin";
 
 const hook = (compiler, cb) => {
   // new webpack
@@ -16,7 +16,7 @@ const hook = (compiler, cb) => {
     return;
   }
   // old webpack
-  compiler.plugin('after-emit', cb);
+  compiler.plugin("after-emit", cb);
 };
 
 module.exports = class WebpackGoogleCloudStoragePlugin {
@@ -26,24 +26,22 @@ module.exports = class WebpackGoogleCloudStoragePlugin {
       include: PropTypes.array,
       exclude: PropTypes.array,
       storageOptions: PropTypes.object.isRequired,
-      uploadOptions: PropTypes.shape(
-        {
-          bucketName: PropTypes.string.isRequired,
-          forceCreateBucket: PropTypes.bool,
-          gzip: PropTypes.bool,
-          public: PropTypes.bool,
-          destinationNameFn: PropTypes.func,
-          metadataFn: PropTypes.func,
-          makePublic: PropTypes.bool,
-          resumable: PropTypes.bool,
-          concurrency: PropTypes.number,
-        }
-      ),
+      uploadOptions: PropTypes.shape({
+        bucketName: PropTypes.string.isRequired,
+        forceCreateBucket: PropTypes.bool,
+        gzip: PropTypes.bool,
+        public: PropTypes.bool,
+        destinationNameFn: PropTypes.func,
+        metadataFn: PropTypes.func,
+        makePublic: PropTypes.bool,
+        resumable: PropTypes.bool,
+        concurrency: PropTypes.number,
+      }),
     };
   }
 
   static get ignoredFiles() {
-    return ['.DS_Store'].map(pattern => new RegExp(pattern));
+    return [".DS_Store"].map((pattern) => new RegExp(pattern));
   }
 
   static defaultDestinationNameFn(file) {
@@ -69,45 +67,41 @@ module.exports = class WebpackGoogleCloudStoragePlugin {
   }
 
   static handleErrors(error, compilation, cb) {
-    compilation.errors.push(
-      new Error(`${pluginName}: ${error.stack}`)
-    );
+    compilation.errors.push(new Error(`${pluginName}: ${error.stack}`));
     cb();
   }
 
   static regexpToIgnoreFunction(regexp) {
-    return file => regexp.test(file);
+    return (file) => regexp.test(file);
   }
 
   constructor(options = {}) {
-    PropTypes.validateWithErrors(
-      this.constructor.schema,
-      options,
-      pluginName
-    );
+    PropTypes.validateWithErrors(this.constructor.schema, options, pluginName);
 
     this.isConnected = false;
 
     this.storageOptions = options.storageOptions;
     this.uploadOptions = options.uploadOptions;
-    this.uploadOptions.destinationNameFn = this.uploadOptions.destinationNameFn ||
+    this.uploadOptions.destinationNameFn =
+      this.uploadOptions.destinationNameFn ||
       this.constructor.defaultDestinationNameFn;
-    this.uploadOptions.metadataFn = this.uploadOptions.metadataFn ||
-      this.constructor.defaultMetadataFn;
+    this.uploadOptions.metadataFn =
+      this.uploadOptions.metadataFn || this.constructor.defaultMetadataFn;
 
-    this.options = pick(
-      options,
-      [
-        'directory',
-        'include',
-        'exclude',
-        'basePath',
-      ]
-    );
+    this.options = pick(options, [
+      "directory",
+      "include",
+      "exclude",
+      "basePath",
+    ]);
 
     // transform the RegExp patterns immediately to minimize regexp object creation
-    this.options.exclude = (this.options.exclude || []).map(pattern => new RegExp(pattern));
-    this.options.include = (this.options.include || []).map(pattern => new RegExp(pattern));
+    this.options.exclude = (this.options.exclude || []).map(
+      (pattern) => new RegExp(pattern)
+    );
+    this.options.include = (this.options.include || []).map(
+      (pattern) => new RegExp(pattern)
+    );
   }
 
   connect() {
@@ -116,12 +110,9 @@ module.exports = class WebpackGoogleCloudStoragePlugin {
     }
 
     this.client = gcs(
-      merge(
-        this.storageOptions,
-        {
-          promise: Promise,
-        }
-      )
+      merge(this.storageOptions, {
+        promise: Promise,
+      })
     );
 
     this.isConnected = true;
@@ -129,39 +120,44 @@ module.exports = class WebpackGoogleCloudStoragePlugin {
 
   filterFiles(files) {
     return Promise.resolve(
-      files.filter(file =>
-        this.isIncluded(file.name) &&
-        !this.isExcluded(file.name) &&
-        !this.isIgnored(file.name)
+      files.filter(
+        (file) =>
+          this.isIncluded(file.name) &&
+          !this.isExcluded(file.name) &&
+          !this.isIgnored(file.name)
       )
     );
   }
 
   isIncluded(fileName) {
-    return this.options.include.some(regexp => fileName.match(regexp));
+    return this.options.include.some((regexp) => fileName.match(regexp));
   }
 
   isExcluded(fileName) {
-    return this.options.exclude.some(regexp => fileName.match(regexp));
+    return this.options.exclude.some((regexp) => fileName.match(regexp));
   }
 
   isIgnored(fileName) {
-    return this.constructor.ignoredFiles.some(regexp => fileName.match(regexp));
+    return this.constructor.ignoredFiles.some((regexp) =>
+      fileName.match(regexp)
+    );
   }
 
   handleFiles(files) {
-    return this.filterFiles(files)
-      .then(filteredFiles => this.uploadFiles(filteredFiles));
+    return this.filterFiles(files).then((filteredFiles) =>
+      this.uploadFiles(filteredFiles)
+    );
   }
 
   apply(compiler) {
     this.connect();
 
     // NOTE: Use specified directory, webpack.config.output or current dir.
-    this.options.directory = this.options.directory ||
-                             compiler.options.output.path ||
-                             compiler.options.output.context ||
-                             '.';
+    this.options.directory =
+      this.options.directory ||
+      compiler.options.output.path ||
+      compiler.options.output.context ||
+      ".";
     hook(compiler, (compilation, cb) => {
       if (this.options.directory) {
         recursive(
@@ -170,15 +166,18 @@ module.exports = class WebpackGoogleCloudStoragePlugin {
           // be compatible
           this.options.exclude.map(this.constructor.regexpToIgnoreFunction)
         )
-          .then(files => files.map(f => ({ name: path.basename(f), path: f })))
-          .then(files => this.handleFiles(files))
+          .then((files) =>
+            files.map((f) => ({ name: path.basename(f), path: f }))
+          )
+          .then((files) => this.handleFiles(files))
           .then(() => cb())
-          .catch(e => this.constructor.handleErrors(e, compilation, cb));
+          .catch((e) => this.constructor.handleErrors(e, compilation, cb));
       } else {
-        this.constructor.getAssetFiles(compilation)
-          .then(files => this.handleFiles(files))
+        this.constructor
+          .getAssetFiles(compilation)
+          .then((files) => this.handleFiles(files))
           .then(() => cb())
-          .catch(e => this.constructor.handleErrors(e, compilation, cb));
+          .catch((e) => this.constructor.handleErrors(e, compilation, cb));
       }
     });
   }
@@ -187,14 +186,17 @@ module.exports = class WebpackGoogleCloudStoragePlugin {
     const bucket = this.client.bucket(this.uploadOptions.bucketName);
     // see https://hackernoon.com/concurrency-control-in-promises-with-bluebird-977249520f23
     // http://bluebirdjs.com/docs/api/promise.map.html#map-option-concurrency
-    return Promise.map(files,
-      file => bucket.upload(file.path, {
-        destination: this.uploadOptions.destinationNameFn(file),
-        gzip: this.uploadOptions.gzip || false,
-        public: this.uploadOptions.makePublic || false,
-        resumable: this.uploadOptions.resumable,
-        metadata: this.uploadOptions.metadataFn(file),
-      }),
-      { concurrency: this.uploadOptions.concurrency || 10 });
+    return Promise.map(
+      files,
+      (file) =>
+        bucket.upload(file.path, {
+          destination: this.uploadOptions.destinationNameFn(file),
+          gzip: this.uploadOptions.gzip || false,
+          public: this.uploadOptions.makePublic || false,
+          resumable: this.uploadOptions.resumable,
+          metadata: this.uploadOptions.metadataFn(file),
+        }),
+      { concurrency: this.uploadOptions.concurrency || 10 }
+    );
   }
 };
